6dc1548addf2d86534cec2d91c8bbd95f27e2bd7
Parsa Ghadimi <me@qti3e.com>
2018-06-29 05:03:16 +0430

    Add ExportAssignment

--- website/testdata/export.ts
+++ website/testdata/export.ts
@@ -5,3 +5,13 @@ const X = 24;
 class Test {}
 
 export { Test as T }
+
+const P = {};
+
+/**
+ * Some valuable doc.
+ * TODO This is already ignored by parser, should be fixed.
+ */
+P.name = {};
+
+export default P;
